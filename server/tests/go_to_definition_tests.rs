84d1e7eb292625e3bf4bdcabe9738ad82a4ae4d2
Adib Farrasy <adibfarrasy@gmail.com>
2026-01-11 17:27:06 +0700

    Fix server test race condition

--- server/tests/go_to_definition_tests.rs
+++ server/tests/go_to_definition_tests.rs
@@ -2,9 +2,8 @@ use std::env;
 use std::sync::Arc;
 
 use pretty_assertions::assert_eq;
-use serial_test::serial;
 use server::{Repository, server::Backend};
-use tokio::sync::OnceCell;
+use tokio::sync::Mutex;
 use tower_lsp::LanguageServer;
 use tower_lsp::lsp_types::{GotoDefinitionResponse, Location, Range};
 use tower_lsp::{
@@ -14,8 +13,9 @@ use tower_lsp::{
         TextDocumentIdentifier, TextDocumentPositionParams, Url, WorkDoneProgressParams,
     },
 };
+use uuid::Uuid;
 
-static TEST_SERVER: OnceCell<TestServer> = OnceCell::const_new();
+static TEST_SERVER: Mutex<Option<TestServer>> = Mutex::const_new(None);
 
 struct TestServer {
     backend: Backend,
@@ -23,8 +23,9 @@ struct TestServer {
 
 impl TestServer {
     async fn new() -> Self {
-        let db_dir = "file::memory:?cache=shared";
-        let repo = Arc::new(Repository::new(db_dir).await.unwrap());
+        let db_name = Uuid::new_v4();
+        let db_dir = format!("file:{}?mode=memory&cache=shared", db_name);
+        let repo = Arc::new(Repository::new(&db_dir).await.unwrap());
         let (service, _socket) = LspService::new(|client| Backend::new(client, repo.clone()));
         let backend = Backend::new(service.inner().client.clone(), repo.clone());
 
@@ -42,14 +43,11 @@ impl TestServer {
     }
 }
 
-async fn get_server() -> &'static TestServer {
-    TEST_SERVER
-        .get_or_init(|| async { TestServer::new().await })
-        .await
+async fn get_server() -> TestServer {
+    TestServer::new().await // Creates NEW TempDir each call
 }
 
 #[tokio::test]
-#[serial(groovy_parser)]
 async fn test_goto_definition_simple() {
     let server = get_server().await;
 
@@ -91,7 +89,6 @@ async fn test_goto_definition_simple() {
 }
 
 #[tokio::test]
-#[serial(groovy_parser)]
 async fn test_goto_definition_static_member() {
     let server = get_server().await;
 
@@ -133,7 +130,6 @@ async fn test_goto_definition_static_member() {
 }
 
 #[tokio::test]
-#[serial(groovy_parser)]
 async fn test_goto_definition_this_member() {
     let server = get_server().await;
 
@@ -146,6 +142,47 @@ async fn test_goto_definition_this_member() {
                     .expect("cannot parse root URI"),
             },
             position: Position::new(42, 14),
+        },
+        work_done_progress_params: WorkDoneProgressParams::default(),
+        partial_result_params: PartialResultParams::default(),
+    };
+
+    let result = server.backend.goto_definition(params).await.unwrap();
+    assert!(result.is_some());
+
+    let location = Location::new(
+        Url::from_file_path(root.join(
+            "/Users/adibf/Projects/lspintar-ws/lspintar/server/tests/fixtures/groovy-gradle-multi/api/src/main/groovy/com/example/api/UserController.groovy",
+        ))
+        .unwrap(),
+        Range {
+            start: Position {
+                line: 18,
+                character: 9,
+            },
+            end: Position {
+                line: 18,
+                character: 16,
+            },
+        },
+    );
+
+    assert_eq!(result.unwrap(), GotoDefinitionResponse::from(location));
+}
+
+#[tokio::test]
+async fn test_goto_definition_this_super_member() {
+    let server = get_server().await;
+
+    let root = env::current_dir().expect("cannot get current dir");
+
+    let params = GotoDefinitionParams {
+        text_document_position_params: TextDocumentPositionParams {
+            text_document: TextDocumentIdentifier {
+                uri: Url::from_file_path(root.join("tests/fixtures/groovy-gradle-multi/api/src/main/groovy/com/example/api/UserController.groovy"))
+                    .expect("cannot parse root URI"),
+            },
+            position: Position::new(45, 14),
         },
         work_done_progress_params: WorkDoneProgressParams::default(),
         partial_result_params: PartialResultParams::default(),
